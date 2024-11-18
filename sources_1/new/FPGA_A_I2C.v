177681863650821346ca3288e9777cb368381bb2
Brian <brian.zagalsky@gmail.com>
2024-11-18 16:40:20 -0500

    Create FPGA_A_I2C.v

---
+++ sources_1/new/FPGA_A_I2C.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2024 04:18:03 PM
+// Design Name: 
+// Module Name: FPGA_A_I2C
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FPGA_A_I2C(
+
+    );
+endmodule
